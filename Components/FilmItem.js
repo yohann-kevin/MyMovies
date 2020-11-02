75825739c8295aa2cfa117fe142ca83c8645f2b0
Perriguey <yo44prg@icloud.com>
2020-11-02 17:37:47 +0100

    feat : add image

--- Components/FilmItem.js
+++ Components/FilmItem.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { StyleSheet, View, Text, Image } from 'react-native'
+import { getImageFromApi } from '../API/TMDBApi'
 
 class FilmItem extends React.Component {
   render() {
@@ -8,7 +9,7 @@ class FilmItem extends React.Component {
       <View style={styles.mainContainer}>
         <Image
           style={styles.image}
-          source={{uri: "image"}}
+          source={{uri: getImageFromApi(film.poster_path)}}
         />
         <View style={styles.contentContainer}>
           <View style={styles.headerContainer}>
