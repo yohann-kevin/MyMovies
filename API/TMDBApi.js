1637f2d7397fc3ce3a5a19dcb7d1666fd22190bc
Perriguey <yo44prg@icloud.com>
2020-12-01 17:33:01 +0100

    feat : add loader in filmDetail

--- API/TMDBApi.js
+++ API/TMDBApi.js
@@ -11,3 +11,10 @@ export function getFilmsFromApiWithSearchedText (text, page) {
 export function getImageFromApi(name) {
     return 'https://image.tmdb.org/t/p/w300' + name;
 }
+
+export function getFilmDetailFromApi() {
+    const url = 'https://api.themoviedb.org/3/movie' + id + '?api_key' + apiToken + '&language=fr';
+    return fetch(url)
+        .then((response) => response.json())
+        .catch((error) => console.error(error))    
+}
--- Components/FilmDetail.js
+++ Components/FilmDetail.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, ActivityIndicator } from 'react-native';
 
 class FilmDetail extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            film: undefined,
+            isLoading: true
+        }
+    }
+
+    _displayLoading() {
+        if (this.state.isLoading) {
+            return (
+                <View style={styles.loadingContainer}>
+                    <ActivityIndicator size="large" />
+                </View>
+            )
+        }
+     }
+
     render() {
-        const idFilm = this.props.navigation.state.params.idFilm
+        // const idFilm = this.props.navigation.state.params.idFilm
         return (
             <View style={ styles.mainContainer }>
-                <Text>Détail du film : { idFilm }</Text>
+                {this._displayLoading()}
             </View>
         )
     }
@@ -15,6 +34,15 @@ class FilmDetail extends React.Component {
 const styles = StyleSheet.create({
     mainContainer: {
         flex: 1,
+    },
+    loadingContainer: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+        alignItems: 'center',
+        justifyContent: 'center'
     }
 })
 
